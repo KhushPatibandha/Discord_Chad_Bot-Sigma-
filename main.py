3b2cad7b31b6f1132b2c0ed820af93b21ea658c3
Khush Patibandha <khush.patibandha@gmail.com>
2023-01-15 16:51:42 +0530

    music streaming with link but cant hear the music

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ def get_quote():
     quote = json_data[0]['q'] +" -"+ json_data[0]['a']
     return quote
 
+voice_clients = {}
+
+yt_dl_opts = {'format' : 'bestaudio/best'}
+ytdl = youtube_dl.YoutubeDL(yt_dl_opts)
+
+ffmpeg_options = {'options' : '-vn'}
+
 @client.event
 async def on_ready():
     print(f"We have logged in as {client.user}")
@@ -42,6 +49,46 @@ async def on_message(msg):
     if(msg.content.startswith('$inspire')):
         await msg.channel.send(get_quote())
 
+    if(msg.content.startswith("$play")):
+
+        try:
+            voice_client = await msg.author.voice.channel.connect()
+            voice_clients[voice_client.guild.id] = voice_client
+        except:
+            print("Error")
+
+        try:
+            url = msg.content.split()[1]
+
+            loop = asyncio.get_event_loop()
+            data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+
+            song = data['url']
+            player = discord.FFmpegPCMAudio(song, **ffmpeg_options)
+
+            voice_clients[msg.guild.id].play(player)
+
+        except Exception as err:
+            print(err)
+
+    if(msg.content.startswith("$pause")):
+        try:
+            voice_clients[msg.guild.id].pause()
+        except Exception as err:
+            print(err)
+
+    if(msg.content.startswith("$resume")):
+        try:
+            voice_clients[msg.guild.id].resume()
+        except Exception as err:
+            print(err)  
+
+    if(msg.content.startswith("$stop")):
+        try:
+            voice_clients[msg.guild.id].stop()
+            await voice_clients[msg.guild.id].disconnect()
+        except Exception as err:
+            print(err)
 
 
 client.run(os.getenv('TOKEN'))
